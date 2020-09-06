d8c41473b5383e62904b079bee3cf7a17bc8eb6e
Julio Lima <julio_flima@hotmail.com>
2020-09-06 05:31:53 -0300

    Backend finished, not tested.

--- packages/backend/controller/SensorController.js
+++ packages/backend/controller/SensorController.js
@@ -1,9 +1,3 @@
-/* eslint-disable no-shadow */
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable no-useless-catch */
-const generateUniqueId = require('@MobileECG/generateUniqueId');
-const connection = require('../database/connection');
-
 const Cloud = require('../model/database/Cloud');
 
 const db = new Cloud('development').connection();
@@ -13,45 +7,23 @@ module.exports = class SensorController {
     try {
       const { name } = req.query;
 
-      const count = await db('todos')
-        .join('lists', 'lists.id', '=', 'todos.lists_id')
+      const count = await db('sensor')
+        .where({
+          name,
+        })
+        .select('*')
         .count()
         .first();
 
-      const todos = await db('todos')
-        .join('lists', 'lists.id', '=', 'todos.lists_id')
-        .select(['todos.*', 'lists.name']);
+      const todos = await db('sensor')
+        .where({
+          name,
+        })
+        .select(['timestamp', 'date', 'value']);
 
-      response.header('X-Total-Count', count['count(*)']);
+      res.header('X-Total-Count', count);
 
-      return response.json(todos);
-
-      const collections = await db.collection(nameCollection).doc(name).listCollections();
-      const timestamps = collections.map((col) => col.id);
-
-      for (const timestamp of timestamps) {
-        snapshots.push(db.collection(nameCollection).doc(name).collection(timestamp).get());
-      }
-
-      const results = await Promise.all(snapshots);
-      results.forEach((snapshot) => {
-        snapshot.forEach((doc) => files.push(doc.data()));
-      });
-
-      res.send(files);
-    } catch (error) {
-      res.send(error);
-    }
-  }
-
-  static async show(req, res) {
-    try {
-      const { name } = req.query;
-
-      const collections = await db.collection(nameCollection).doc(name).listCollections();
-      const collectionIds = collections.map((col) => col.id);
-
-      res.send(collectionIds);
+      return res.status(200).json(todos);
     } catch (error) {
       res.send(error);
     }
@@ -61,69 +33,38 @@ module.exports = class SensorController {
     try {
       const { name } = req.query;
       const { date, value, timestamp } = req.body;
-      const data = { date, value, timestamp };
-      const nameSubCollection = Number(timestamp).toString();
 
-      const response = await db
-        .collection(nameCollection)
-        .doc(name)
-        .collection(nameSubCollection)
-        .doc()
-        .set(data);
+      await connection('todos').insert({
+        name,
+        date,
+        value,
+        timestamp,
+      });
 
-      res.status(200).send(response);
+      return res
+        .status(200)
+        .json({ message: `Timestamp "${timestamp}", of sensor "${name}"  added! Id:`, id });
     } catch (error) {
       throw error;
     }
   }
 
   static async delete(req, res) {
-    const deleteCollection = async (db, collectionPath, batchSize) => {
-      const deleteQueryBatch = async (db, query, resolve) => {
-        try {
-          const snapshot = await query.get();
-
-          const batchSize = snapshot.size;
-          if (batchSize === 0) {
-            resolve();
-            return;
-          }
-
-          const batch = db.batch();
-          snapshot.docs.forEach((doc) => {
-            batch.delete(doc.ref);
-          });
-          await batch.commit();
-
-          process.nextTick(() => {
-            deleteQueryBatch(db, query, resolve);
-          });
-        } catch (error) {
-          throw error;
-        }
-      };
-
-      try {
-        const collectionRef = db.collection(collectionPath);
-        const query = collectionRef.orderBy('__name__').limit(batchSize);
-
-        return new Promise((resolve, reject) => {
-          deleteQueryBatch(db, query, resolve).catch(reject);
-        });
-      } catch (error) {
-        throw error;
-      }
-    };
-
     try {
       const { name } = req.query;
       const { timestamp } = req.body;
-      const nameSubCollection = Number(timestamp).toString();
-      const path = `/${nameCollection}/${name}/${nameSubCollection}`;
 
-      await deleteCollection(db, path, 1);
+      const data = await connection('sensor').where({ name, timestamp }).select('*').first();
 
-      res.status(200).send(`Timestamp "${timestamp}", of sensor "${name}"  deleted!`);
+      if (!data) {
+        return res.status(404).json({ error: 'Was not found that data.' });
+      } else {
+        await connection('sensor').where({ name, timestamp }).delete();
+
+        return res
+          .status(200)
+          .json({ message: `Timestamp "${timestamp}", of sensor "${name}"  deleted!` });
+      }
     } catch (error) {
       throw error;
     }
--- packages/backend/model/database/migrations/20200814050450_create_todos.js
+++ packages/backend/model/database/migrations/20200814050450_create_todos.js
@@ -1,6 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('sensor', (table) => {
     table.string('date').notNullable();
+    table.string('name').notNullable().primary();
     table.string('timestamp').notNullable().primary();
     table.string('value').notNullable().primary();
   });
